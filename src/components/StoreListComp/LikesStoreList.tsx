0715d8570d487717a69eb7eb7930f122784cda83
nr-baek <nara130825@gmail.com>
2023-02-08 12:19:32 +0900

    찜한 가게 조회 전 로그인 확인 방식 변경
    alert -> confirm 방식으로 변경
    페이지 이동 취소 가능하도록 개선함

--- src/components/StoreListComp/LikesStoreList.tsx
+++ src/components/StoreListComp/LikesStoreList.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { StyledContainer } from "./styles";
 import StoreItem from "./StoreItem";
-import { useNavigate } from "react-router-dom";
 import { useStoreListOfLikeQuery } from "hooks/storeListQuery";
 import { useUserId } from "store";
 import { Message } from "components/common/styles";
 
 function LikesStoreList() {
-  const navigate = useNavigate();
   const id = useUserId();
 
   const {
@@ -16,13 +14,6 @@ function LikesStoreList() {
     isError,
     error,
   } = useStoreListOfLikeQuery(id);
-
-  useEffect(() => {
-    if (!id) {
-      alert("로그인이 필요한 페이지입니다.");
-      navigate("/login");
-    }
-  }, []);
 
   if (isLoading || isError) {
     return (
--- src/components/common/BottomMenu.tsx
+++ src/components/common/BottomMenu.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { MenuWrapper } from "./styles";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useUserId } from "store";
 
 function BottomMenu() {
   const { pathname } = useLocation();
+  const id = useUserId();
+  const navigate = useNavigate();
 
   const isHome =
     pathname === "/" ||
@@ -20,6 +23,18 @@ function BottomMenu() {
   const isLikes = pathname === "/likes";
   const isMypage = pathname === "/mypage";
 
+  const onClickLikes = useCallback(
+    (e: React.MouseEvent<HTMLAnchorElement>) => {
+      if (!id) {
+        e.preventDefault();
+        if (window.confirm("로그인이 필요합니다. 로그인하시겠습니까?")) {
+          navigate("/login");
+        }
+      }
+    },
+    [id, navigate],
+  );
+
   return (
     <MenuWrapper>
       <ul>
@@ -30,7 +45,7 @@ function BottomMenu() {
           </Link>
         </li>
         <li>
-          <Link to="/likes">
+          <Link to="/likes" onClick={onClickLikes}>
             <span className="a11y-hidden">찜한 가게</span>
             <i
               className={`icon fa-solid fa-heart ${isLikes ? "select" : ""}`}
